16dada4540c8b69ad2c5e5b77c9917efe74bb444
ajevans <ajevans@PC3568>
2023-02-01 16:08:42 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeployNET6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
